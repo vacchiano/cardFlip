a374758374b0346453c9de9418580e0ac13d58b0
Dom Vacchiano <djvacchiano@gmail.com>
2020-12-22 22:36:59 -0600

    updated name to sports card flip

--- cardflip/cardflip/settings.py
+++ cardflip/cardflip/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$atr=#uv3k$l*qf)ie35almi%6s3xsp@v5pdn*5bl&l(iuxxv8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
